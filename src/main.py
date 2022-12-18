6282fe0814334b6a70b766c6c0a87b40065604ad
Aaron Roethe <roethe.aaron@outlook.com>
2022-12-17 23:03:42 -0700

    add secrets & install scripts
    Signed-off-by: Aaron Roethe <roethe.aaron@outlook.com>

--- src/main.py
+++ src/main.py
@@ -1,5 +1,27 @@
+import json
+import os
+
+from functions import arguments, helpers
+
+args = arguments.handleArgs()
+
+# Secrets
+# ====================================================================================
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+
+# Initialization
+# ====================================================================================
+helpers.checkInstalledPackages()
+
+config = helpers.read_json(f'configs/{args.file}')['client']
+
+logger = helpers.log(args)
+
+# Debug - Display Config Dictionary
+logger.debug(json.dumps(config, indent=4))
+
 def main():
-    print('hello new project')
+    pass
 
 if __name__ == '__main__':
     main()
